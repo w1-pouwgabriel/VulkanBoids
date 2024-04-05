27c804787b2c68c93ac57de0e6112c2cc1f68708
Gabriël Pouw <gabriel@pouw.com>
2024-04-05 23:01:06 +0200

    Store all the image information in 1 place

--- Source/Runtime/Graphics/Public/Frame.h
+++ Source/Runtime/Graphics/Public/Frame.h
@@ -2,11 +2,10 @@
 #include "Utils.h"
 
 namespace VkUtil{
-    /**
-		Store all the image information in 1 place
-	*/
+    
 	struct SwapChainFrame{
 		vk::Image image;
 		vk::ImageView imageView;
 	};
+	
 }
